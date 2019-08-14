8f0c6d965ca4b2bc278b7d4422156d66ad736126
Layerdev <layerdev@protonmail.com>
2019-08-14 14:18:21 +0200

    add link to original text

--- src/components/listWithOptions/index.js
+++ src/components/listWithOptions/index.js
@@ -30,7 +30,14 @@ const ListWithOptions = props => {
     const setCurrentRcv = async e => {
         await getVotation(e.target.id);
     }
+
     
+    const createLink = id => {
+        const transformer = /(\w)(\d)-(\d{4})\/(\d{4})/;
+        let textLink = id.replace(transformer, '$1-$2-$4-$3');
+        return  `http://www.europarl.europa.eu/doceo/document/${textLink}_EN.html`;
+    };
+
     return (
         <section className={styles.container}>
         {textLoading 
@@ -59,6 +66,16 @@ const ListWithOptions = props => {
                                 <button 
                                     onClick={flushCurrentVotations}
                                 >BACK</button>
+
+                                    <a
+                                        href={createLink(currVotations[0].ABNumber)}
+                                        target="_blank"
+                                        rel="noopener noreferrer"
+                                    >
+                                                <li className={styles.firstListItem}>
+                                        READ TEXT
+                                </li>
+                                    </a>
                                 {currVotations.map(rcv => 
                                     <li
                                         key={rcv._id}
